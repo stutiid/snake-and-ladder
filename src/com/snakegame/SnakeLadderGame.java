bcbbb45ccd59c44e5c75c98bf82388a0e8a2dec3
stutiid <stutichaturvedi12@gmail.com>
2021-03-23 18:01:00 +0530

    Added: counting the number of times dice is rolled

--- src/com/snakegame/SnakeLadderGame.java
+++ src/com/snakegame/SnakeLadderGame.java
@@ -2,33 +2,42 @@ package com.snakegame;
 
 import java.util.Random;
 
-public class SnakeLadderGame {
-	public static void main(String args[]) {
+public class SnakeLadderGame 
+{
+	public static void main(String args[]) 
+	{
 		System.out.println("Welcome to snake and ladder simulator");
 		// variables and objects
 		int position = 0;// initial position of player
 		Random rand = new Random(); // using Random class of java.util
 		int option;
+		int count = 0;
 		// printing initial position of player
 		System.out.println("your initial position is " + position);
 		// loop till player reach the winning spot which is 100
-		while (position < 100) {
+		while (position < 100) 
+		{
 			int dice = rand.nextInt(6) + 1; // dice number generator
+			count++;// counting the number of times dice was played
 			System.out.println("dice " + dice);
 			option = rand.nextInt(3) + 1; // option generator
 			System.out.println("option " + option);
 			// checking out the options
 			if (option == 1)
 				System.out.println("NO Play, your position is same as before " + position);
-			else if (option == 2) {
+			else if (option == 2) 
+			{
 				int tempPosition = position + dice;
 				if (tempPosition > 100) // need exact 100 to win
 					System.out.println("You need to get exact 100 your current positon is same as before " + position);
-				else {
+				else 
+				{
 					position = tempPosition; // updating the position
 					System.out.println("you got ladder, your current position is = " + position);
 				}
-			} else {
+			} 
+			else 
+			{
 				int tempPosition = position - dice;
 				if (tempPosition < 0) // if tempPosition is negative then you have to restart
 					position = 0;
@@ -37,5 +46,6 @@ public class SnakeLadderGame {
 				System.out.println("You got bitten by snake, your current position is =" + position);
 			}
 		}
+		System.out.println("total number of times dice is played to win the game " + count);
 	}
 }
