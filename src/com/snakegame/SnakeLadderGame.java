207f2ab9d78cd5e98aa7f559e64e7d8ade537b58
stutiid <stutichaturvedi12@gmail.com>
2021-03-23 15:57:57 +0530

    Added:Rolled a dice

--- src/com/snakegame/SnakeLadderGame.java
+++ src/com/snakegame/SnakeLadderGame.java
@@ -1,8 +1,16 @@
 package com.snakegame;
 
+import java.util.Random;
+
 public class SnakeLadderGame {
 public static void main(String args[]) {
 	System.out.println("Snake and ladder game simulator");
-	System.out.println("Single player at position 0");
+	//variables and objects
+	int position=0;
+	Random rand=new Random();
+	//computation
+	int dice=rand.nextInt(6)+1;
+	System.out.println(dice);
+	
 }
 }
