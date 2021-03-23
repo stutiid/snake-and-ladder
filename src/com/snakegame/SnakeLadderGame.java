b93f0306f5873bb5c3e6c31dbe0706177583a793
stutiid <stutichaturvedi12@gmail.com>
2021-03-23 09:53:21 +0530

    Added: snake and ladder simulator

---
+++ src/com/snakegame/SnakeLadderGame.java
@@ -0,0 +1,8 @@
+package com.snakegame;
+
+public class SnakeLadderGame {
+public static void main(String args[]) {
+	System.out.println("Snake and ladder game simulator");
+	System.out.println("Single player at position 0");
+}
+}
